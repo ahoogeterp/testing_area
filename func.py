0fdbb8036f0bbd84a923eddc66e4b011e681aa45
ahoogeterp <outdoorsunlmtd@gmail.com>
2022-11-11 15:43:49 -0600

    a

--- func.py
+++ func.py
@@ -1,7 +1,12 @@
-from time import sleep
-from PIL import Image
 import os
+from time import sleep
+import collections
 import natsort
+from pdf2image import convert_from_path
+from PIL import Image
+import pdfkit
+import pytesseract
+import numpy as np
 
 def counting():
     c = 0
@@ -11,7 +16,8 @@ def counting():
         sleep(1)
         continue
 
-def img_con(src, dest):
+
+def img_con_pdf(src, dest):
     # print(src, dest)
     img_in = (natsort.natsorted(os.listdir(src)))
     try:
@@ -20,5 +26,71 @@ def img_con(src, dest):
             im_1 = image_1.convert('RGB')
             im_1.save(dest+f.split('.')[-2]+'.pdf')
             continue
+    except:
+        print(Exception)
+
+def pdf_con_img(src_path, dest_path):
+    pages = convert_from_path(src_path, 350)
+    i = 1
+    try:
+        for page in pages:
+            image_name = "Page_" + str(i) + ".jpg"  
+            page.save(dest_path+image_name, "JPEG")
+            i = i+1  
+    except:
+        print(Exception)
+
+
+def pdf_con_txt():
+    print('')
+
+def www_to_pdf(src_page):
+    pdfkit.from_url(src_page, 'out.pdf')
+
+def img_to_txt(src_path):
+    img_lst = (natsort.natsorted(os.listdir(src_path)))
+    for i in img_lst:# tesseract converting images to plain txt format image by image to create one text file
+        text = pytesseract.image_to_string(np.array(Image.open(src_path+i)))
+        txt_name = f'{src_path}text.txt'     #.split (".")[-2]                   
+        with open(txt_name, 'a+', encoding='utf-8') as f:
+            f.write(text)
+            f.close()
+
+
+
+def text_count(src_path,):
+    try:
+        # Create an empty dictionary
+        d = dict()
+
+        # Loop through each line of the file
+        for line in open(src_path, "r"):
+            # Remove the leading spaces and newline character
+            line = line.strip(line.lower(line.split(" ")))
+            # line = line.strip()
+
+            # Convert the characters in line to
+            # lowercase to avoid case mismatch
+            # line = line.lower()
+
+            # Split the line into words
+            # words = line.split(" ")
+                                
+
+            # Iterate over each word in line
+            for word in line:
+                # Check if the word is already in dictionary
+                if word in d:
+                    # Increment count of word by 1
+                    d[word] = d[word] + 1
+                else:
+                    # Add the word to dictionary with count 1
+                    d[word] = 1
+
+        # Print the contents of dictionary
+        for key in sorted(list(d.keys())):
+            print(key, ":", d[key])
+
+        print(d)
     except:
         print(Exception)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from func import counting, img_con
+from func import counting, img_con_pdf, pdf_con_img, text_count, img_to_txt
+
 
 # counting()
-source = r'/foo/'
-destination = r'/foo/'
-img_con(source, destination)
+source = r'/home/adam/repos/testing_area/src/out.pdf'
+source1 = r'/home/adam/repos/testing_area/dest/' 
+destination = r'/home/adam/repos/testing_area/dest/'
+# img_con_pdf(source, destination)
+
+# pdf_con_img(source, destination)
+
+
+# text_count()
+img_to_txt(source1)
