3a308d08582706f01fb4bd0e012f475ed2a1cb77
ahoogeterp <outdoorsunlmtd@gmail.com>
2022-11-11 13:49:19 -0600

    func testing

--- func.py
+++ func.py
@@ -1,1 +1,24 @@
-import time
+from time import sleep
+from PIL import Image
+import os
+import natsort
+
+def counting():
+    c = 0
+    while c != 20:
+        c += 1
+        print(c)
+        sleep(1)
+        continue
+
+def img_con(src, dest):
+    # print(src, dest)
+    img_in = (natsort.natsorted(os.listdir(src)))
+    try:
+        for f in img_in:
+            image_1 = Image.open(src+f)
+            im_1 = image_1.convert('RGB')
+            im_1.save(dest+f.split('.')[-2]+'.pdf')
+            continue
+    except:
+        print(Exception)
---
+++ main.py
@@ -0,0 +1,6 @@
+from func import counting, img_con
+
+# counting()
+source = r'/foo/'
+destination = r'/foo/'
+img_con(source, destination)
