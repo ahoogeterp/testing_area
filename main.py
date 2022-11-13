437983efa6bfbaae4756cbd83e9ca6b9b024adc0
ahoogeterp <outdoorsunlmtd@gmail.com>
2022-11-12 18:21:14 -0600

    aaa

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from func import counting, img_con_pdf, pdf_con_img, text_count, img_to_txt, tex
 
 
 # counting()
-source = r'/home/adam/repos/testing_area/src/out.pdf'
-source1 = r'/home/adam/repos/testing_area/dest/' 
-destination = r'/home/adam/repos/testing_area/dest/'
+source = r'/foo/out.pdf'
+source1 = r'/foo/dest/' 
+destination = r'/foo/dest/'
 # img_con_pdf(source, destination)
 
 # pdf_con_img(source, destination)
@@ -12,4 +12,4 @@ destination = r'/home/adam/repos/testing_area/dest/'
 
 # text_count()
 # img_to_txt(source1)
-text_count(r'/home/adam/repos/testing_area/dest/text.txt')
+text_count(r'/foo/text.txt')
